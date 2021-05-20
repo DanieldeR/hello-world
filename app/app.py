dd9d2e0f509204bd7c920987e4751077059847f2
DanieldeR <derepentigny.daniel@gmail.com>
2021-05-20 17:05:44 -0400

    Updating to gunicorn

--- app/app.py
+++ app/app.py
@@ -1,13 +1,13 @@
 from flask import Flask
 from flask.blueprints import Blueprint
 
-api_app = Blueprint("api", __name__)
+app = Flask(__name__)
 
 
-@api_app.route("/")
+@app.route("/")
 def home_route():
     return "Hello World"
 
 
 if __name__ == "__main__":
-    api_app.run()
+    app.run()
